9a243b31eb5e1a5c65605edb03f5f3326a6e5500
x-poison <abdulrahmanmossadhacker@gmail.com>
2023-10-16 12:08:54 -0400

    Bookclub Web Design

--- reset.php
+++ reset.php
@@ -1,32 +1,73 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $phone = $_POST["phone"];
-    $email = $_POST["email"];
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Reset</title>
+</head>
+<body>
 
-    $conn = new mysqli('localhost', 'root', 'abdul', 'school');
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $newPassword = generateRandomPassword(); // Function to generate a random password
+
+    // TODO: Validate email and phone number (you can use regular expressions for validation)
+
+    // TODO: Connect to your database
+    $servername = "localhost";
+    $username = "your_username";
+    $password = "your_password";
+    $dbname = "your_database_name";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
     if ($conn->connect_error) {
-        die('Connection Failed!!' . $conn->connect_error);
+        die("Connection failed: " . $conn->connect_error);
     }
 
-    $stmt = $conn->prepare("SELECT * FROM people WHERE phone = ? AND email = ?");
-    $stmt->bind_param("ss", $phone, $email);
-    $stmt->execute();
-    $result = $stmt->get_result();
+    // TODO: Check if the email and phone number exist in the database
+    $sql = "SELECT * FROM users WHERE email='$email' AND phone_number='$phone'";
+    $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
-        $stmt = $conn->prepare("DELETE FROM people WHERE phone = ? AND email = ?");
-        $stmt->bind_param("ss", $phone, $email);
-        $stmt->execute();
-        
-        echo "User data deleted successfully! Please <a href='register.html'>register again</a>.";
+        // User found, update the password
+        $hashedPassword = password_hash($newPassword, PASSWORD_DEFAULT);
+        $updateSql = "UPDATE users SET password='$hashedPassword' WHERE email='$email' AND phone_number='$phone'";
+
+        if ($conn->query($updateSql) === TRUE) {
+            echo "Password reset successful. Your new password is: $newPassword";
+        } else {
+            echo "Error updating password: " . $conn->error;
+        }
     } else {
-        echo "No matching record found for the provided email and phone number.";
+        echo "User not found. Password reset failed.";
     }
 
-    $stmt->close();
     $conn->close();
-} else {
-    header("Location: reset_pass.html");
-    exit();
+}
+
+function generateRandomPassword($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $randomPassword = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomPassword .= $characters[rand(0, strlen($characters) - 1)];
+    }
+    return $randomPassword;
 }
 ?>
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <label for="email">Email:</label>
+    <input type="email" name="email" required><br><br>
+
+    <label for="phone">Phone Number:</label>
+    <input type="text" name="phone" required><br><br>
+
+    <button type="submit">Reset Password</button>
+</form>
+
+</body>
+</html>
