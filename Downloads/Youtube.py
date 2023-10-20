f4fbbfbba11481fa935d4669a51da0ba9ad08096
x-poison <abdulrahmanmossadhacker@gmail.com>
2023-10-20 15:51:50 -0400

    Bookclub Web Design

--- Downloads/Youtube.py
+++ Downloads/Youtube.py
@@ -1,12 +1,13 @@
 import yt_dlp
 import os
+from colorama import Fore
 
 while True:
     try:
         url = input('Give me Link (or type "exit" to quit): ')
         
         if url.lower() == 'exit':
-            print('Bye...')
+            print(Fore.CYAN+ 'Bye...')
             break
         ydl_opts = {
             'outtmpl': os.path.join(os.path.expanduser("~"), "Downloads", "%(title)s.%(ext)s")
@@ -14,7 +15,7 @@ while True:
         
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
-        print('Success! Video downloaded.')
+        print(Fore.CYAN+ 'Success! Video downloaded.')
         
     except Exception as e:
-        print(f'Error: {e}')
+        print(Fore.CYAN+ f'Error: {e}')
