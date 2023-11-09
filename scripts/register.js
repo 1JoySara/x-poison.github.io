7a22a7b59b05ee9a074aad18d507d415e73edf56
x-poison <abdulrahmanmossadhacker@gmail.com>
2023-11-08 23:28:00 -0500

    commit

--- scripts/register.js
+++ scripts/register.js
@@ -7,11 +7,22 @@ document.getElementById("form").addEventListener('submit', function(event){
     var password = document.getElementById("password_1").value;
     var password_2 = document.getElementById("password_2").value;
     const password_error = "Passwords don't match";
+    const name_error = "Check those names you entered";
+    const name_digit = "Names should not contain digits";
     const password_requirements_error = "Password must be at least 8 characters long, contain at least one special character, one uppercase letter, and four digits.";
-    var passwordPattern = /^(?=.*[!@#$%^&*(),.?":{}|<>])(?=.*\d{4,})(?=.*[A-Z]).{8,}$/;
+    const passwordPattern = /^(?=.*[!@#$%^&*(),.?":{}|<>])(?=.*\d{4,})(?=.*[A-Z]).{8,}$/;
+    const namePattern = /^[A-Za-z]+$/;
 
     if (!passwordPattern.test(password)) {
         alert(password_requirements_error);
+        event.preventDefault();
+    } else if (password !== password_2) {
+        alert(password_error);
+        event.preventDefault();
+    }
+
+    if (!namePattern.test(first_Name) || !namePattern.test(second_Name)) {
+        alert(name_error);
         event.preventDefault();
     } else if (password !== password_2) {
         alert(password_error);
